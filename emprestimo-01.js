bd849eac21d034134baaad342ad2fa9aa8052613
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-10-05 20:47:34 -0300

    Corrigir condicoes da primeira solucao do Exercicio 1, adicionar uma nova solucao ao Exercicio 1

--- emprestimo-01.js
+++ emprestimo-01.js
@@ -2,7 +2,7 @@ let idade = prompt ('Qual sua idade?');
 let renda = prompt('Agora nos diga, por favor, qual sua renda?');
 let emprestimo = prompt('E quanto você gostaria de pegar emprestado?');
 
-if (idade >= 25 && idade <= 65 && renda >= 1500 && emprestimo > 1000 && emprestimo < renda * 10) {
+if (idade >= 25 && idade <= 65 && renda >= 1500 && emprestimo >= 1000 && emprestimo <= renda * 10) {
     alert('Parabéns, você foi aprovado para o nosso empréstimo!');
 }
 else {
---
+++ emprestimo-02.js
@@ -0,0 +1,19 @@
+let idade = prompt ('Qual sua idade?');
+let renda = prompt('Agora nos diga, por favor, qual sua renda?');
+let emprestimo = prompt('E quanto você gostaria de pegar emprestado?');
+
+if (idade < 25 || idade > 65) {
+    alert('Desculpe, mas nós buscamos clientes entre 25 e 65 anos.');
+}
+else {
+    if  (renda < 1500) {
+        alert('Desculpe, mas nós buscamos clientes com renda acima de R$1500,00.');
+    } else {
+        if (emprestimo < 1000 || emprestimo > renda * 10) {
+            alert(`Desculpe, mas nós só fazemos empréstimos para você entre R$1500,00 e R$${renda*10},00.`);
+        }
+        else {
+            alert('Parabéns, você foi aprovado para o nosso empréstimo!');
+        }
+    }
+}
